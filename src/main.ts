de13b50c16325c073aeddb4ac4e750591b4c8e98
nieaowei <nieaowei@gmail.com>
2020-08-09 14:59:51 +0800

    init commit.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios, { AxiosStatic } from 'axios'
 
+Vue.prototype.$axios = axios
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $axios: AxiosStatic;
+  }
+}
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <el-button type="success" v-on:click="test"></el-button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default Vue.extend({
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {},
+  methods: {
+    test (): void {
+      console.log('123')
+      this.$axios('http://www.baidu.com')
+    }
   }
 })
 </script>
